718ee0e368468569e99266be6a9cfeb9477ef518
Phung Tuan Hoang <phungtuanhoang1996@gmail.com>
2018-04-16 01:09:11 +0800

    some ui changes added a check using the last character whether the string has been correctly transmitted

--- app/src/main/java/com/example/vitquay/myownsoundtranfer/ReceiverActivity.kt
+++ app/src/main/java/com/example/vitquay/myownsoundtranfer/ReceiverActivity.kt
@@ -15,13 +15,17 @@ import android.support.v4.content.ContextCompat
 /**
  * Created by Phung Tuan Hoang on 6/4/2018.
  */
+
 class ReceiverActivity: Activity() {
+    var lastTimeStringDecoded:Long = 0
+    var prevDecodedString: String = ""
     var soundSampler: SoundSampler? = null
     var fftThread: Thread? = null
     var fftArray: DoubleArray? = null
     var handler: Handler = Handler(Looper.getMainLooper())
     var monitorFreq: Int = 0
     var peakArrayList: ArrayList<Int> = ArrayList()
+    var characterCount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +33,12 @@ class ReceiverActivity: Activity() {
         setContentView(R.layout.activity_receiver)
 
         setupPermission()
+
+        resetButton.setOnClickListener {
+            peakTextView.text = ""
+            prevDecodedString = ""
+            lastTimeStringDecoded = System.currentTimeMillis()
+        }
 
         soundSampler = SoundSampler(44100, 44100)
         try {
@@ -48,42 +58,99 @@ class ReceiverActivity: Activity() {
                     doubleFFT_1D.realForward(fftArray)
                     informListener(fftArray!!)
                     handler.post{
-                        calculatePeak(2000, 2400, 3)
+                        calculatePeak(500, 6000, 21)
                     }
-                    sleep(16)
+                    sleep(30)
                 }
             }
         }
         fftThread!!.start()
-
-        addMonitorFreqButton.setOnClickListener {
-//            try {
-//                monitorFreq = addFreqEditText.text.toString().toInt()
-//            } catch (e: Exception) {
-//                Toast.makeText(this, "Error converting string to int", Toast.LENGTH_LONG).show();
-//            }
-        }
     }
 
     private fun calculatePeak(startFreq: Int, endFreq: Int, peakCount: Int) {
+        var values = ArrayList<Pair>()
 
-            var tempString = ""
-            var values = ArrayList<Pair>()
-
-            for (i in startFreq .. endFreq) {
-                values.add(Pair(i, Math.log((Math.sqrt(Math.pow(fftArray!![2 * i], 2.0) + Math.pow(fftArray!![2 * i + 1], 2.0)) ))))
-            }
-
-            values.sortDescending()
-
-            for (i in 0 until peakCount) {
-                tempString += values[i].frequency.toString() + " "
-            }
-
-        handler.post{
-            peakTextView.text = tempString
+        for (i in startFreq .. endFreq) {
+            var magnitude = Math.log((Math.sqrt(Math.pow(fftArray!![2 * i], 2.0) + Math.pow(fftArray!![2 * i + 1], 2.0)) ))
+            values.add(Pair(i, magnitude))
         }
 
+        values.sortDescending()
+
+        var charArray = IntArray(peakCount)
+        lateinit var pair:IntegerPair
+//        for (i in 0 until peakCount) {
+//            if (values[i].magnitude >15) {
+//                pair = calculatePrimeCoefficients(13, 23, values[i].frequency)
+//                if (pair.coeff1 < charArray.size) charArray[pair.coeff1] = pair.coeff2.toChar()
+//            } else {
+//                i--
+//            }
+//        }
+
+        var i = 0
+        var arrayCounter = 0
+        var pseudoHash: Float = 0f
+
+        var temp: String = ""
+
+        while (i < peakCount && arrayCounter < values.size) {
+            if (values[arrayCounter].magnitude > 15) {
+                pair = calculatePrimeCoefficients(83, 23, values[arrayCounter].frequency)
+                //println("A peak at " + values[arrayCounter].frequency.toString())
+                if (pair.coeff1 < charArray.size) {
+                    charArray[pair.coeff1] = pair.coeff2
+                }
+                i++
+                pseudoHash = values[arrayCounter].frequency.toFloat()
+            }
+            arrayCounter++
+        }
+
+        var sum: Int = 0
+        var average: Float = 0f
+        for ( i in 0 until charArray.size) {
+            if (i != charArray.size - 1) {
+                if (charArray[i] < 32 || charArray[i] > 126) return
+                sum += charArray[i];
+            }
+            if (i == charArray.size - 1) {
+                average = charArray[i].toFloat()
+            }
+        }
+
+        if (average - (sum / (charArray.size - 1).toFloat()) < 0.001 && average != 0f) {
+            handler.post {
+                var secondCharArray  = CharArray(peakCount - 1)
+                for ( i in 0 until secondCharArray.size) {
+                    secondCharArray[i] = charArray[i].toChar()
+                }
+                var decodedString = String(secondCharArray)
+                if (!decodedString.equals(prevDecodedString)) {
+                    peakTextView.text = peakTextView.text.toString() + decodedString
+                    lastTimeStringDecoded = System.currentTimeMillis()
+                    prevDecodedString = decodedString
+                } else if (System.currentTimeMillis() - lastTimeStringDecoded > 2000) {
+                    peakTextView.text = peakTextView.text.toString() + decodedString
+                    lastTimeStringDecoded = System.currentTimeMillis()
+                    prevDecodedString = decodedString
+                }
+            }
+        }
+
+    }
+
+    private fun calculatePrimeCoefficients(prime1: Int, prime2: Int, number: Int): ReceiverActivity.IntegerPair {
+        var i = 0
+
+        while (i * prime1 <= number) {
+            if ((number - i * prime1) % prime2 == 0) {
+                return IntegerPair(i, (number - i * prime1) / prime2)
+            } else {
+                i++
+            }
+        }
+        return IntegerPair(0, 0)
     }
 
     fun shortArrayToDoubleArray(doubleArray: DoubleArray, shortArray: ShortArray) {
@@ -131,5 +198,15 @@ class ReceiverActivity: Activity() {
                 else -> 0
             }
         }
+    }
+
+    class IntegerPair(var coeff1: Int, var coeff2: Int) {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        fftThread!!.interrupt()
+        soundSampler!!.stop()
     }
 }
